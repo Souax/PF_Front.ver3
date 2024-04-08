3aa30ba541eaae7a20cf13189d4ff7bca4b89c6e
宮下颯太 <sou@MacBook-Air-M2.local>
2024-04-08 19:22:25 +0900

    login

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,55 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+//import GitHubProvider from "next-auth/providers/github";
+import axios from "axios";
+
+const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
+
+type ClientType = {
+  clientId: string;
+  clientSecret: string;
+  user: {
+    email: string;
+  };
+};
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    } as ClientType),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account }): Promise<string | boolean> {
+      const provider = account?.provider;
+      const uid = account?.sub;
+      const name = user?.name;
+      const email = user?.email;
+
+      try {
+        const response = await axios.post(
+          `${apiUrl}/auth/${provider}/callback`,
+          {
+            provider,
+            uid,
+            name,
+            email,
+          }
+        );
+
+        if (response.status === 200) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        console.log("エラー", error);
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
