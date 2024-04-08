b03a25eca36c08d6706216b0a092b93e76a09d4c
宮下颯太 <sou@MacBook-Air-M2.local>
2024-04-08 19:27:43 +0900

    toppage

---
+++ src/TopPageAPI.ts
@@ -0,0 +1,49 @@
+import { ApiResponse } from "../types/types";
+
+/* 新刊書の取得 */
+export const getNewBooks = async (page: number = 1): Promise<ApiResponse> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/newbook`, {
+    next: { revalidate: 1800 },
+  });
+  const newbooks = await res.json();
+  return newbooks;
+};
+
+/* 人気技術書の取得 */
+export const getPopularityBooks = async (
+  page: number = 1
+): Promise<ApiResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/popularity?page=${page}`,
+    {
+      next: { revalidate: 1800 },
+    }
+  );
+  const popularitybooks = await res.json();
+  return popularitybooks;
+};
+
+export const getPopularityAllBooks = async (
+  page: number = 1
+): Promise<ApiResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/popularity?page=${page}`,
+    {
+      next: { revalidate: 1800 },
+    }
+  );
+  const popularityallbooks = await res.json();
+  return popularityallbooks;
+};
+
+/* おすすめ技術書の取得 */
+export const getRecommendationBooks = async (): Promise<ApiResponse> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/recommendation`,
+    {
+      next: { revalidate: 1800 },
+    }
+  );
+  const recommendationbooks = await res.json();
+  return recommendationbooks;
+};
